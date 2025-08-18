1d4f3b1be73e650fd3ba67558cfd756cb73a31d0
Sounak Mitra <sounak1997>
2025-08-18 22:09:08 +0530

    changes for ngrx

--- sounak-backend/src/middleware/loggerMiddleware.js
+++
@@ -1,8 +0,0 @@
-// my-angular-node-backend/src/middleware/loggerMiddleware.js
-
-const loggerMiddleware = (req, res, next) => {
-  console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
-  next(); // Pass control to the next middleware/route handler
-};
-
-module.exports = { loggerMiddleware }; // Correctly export the function
